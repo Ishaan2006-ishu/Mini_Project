6c9075eb6e75ade777c6ec1faa65cba8b6cc1587
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2026-03-14 22:05:47 +0530

    rigister and login api

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const User = require("../models/user")
 
 const router = express.Router()
@@ -33,6 +34,53 @@ router.post("/register", async (req, res) => {
 
     res.json({
       message: "User registered successfully"
+    })
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: "Server error",
+      error: error.message
+    })
+
+  }
+
+})
+
+router.post("/login", async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body
+
+    // find user
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found"
+      })
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid password"
+      })
+    }
+
+    // create JWT token
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    )
+
+    res.json({
+      message: "Login successful",
+      token
     })
 
   } catch (error) {
--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/api", authRoutes)
+app.use("/api/auth", authRoutes)
 
 
 mongoose.connect(process.env.MONGO_URI)
